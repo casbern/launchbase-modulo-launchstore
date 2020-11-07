5cbb92742756facb855b02b7afda4b3cd2f6bbfd
Cássia <cascrisbern@gmail.com>
2020-11-06 22:09:54 -0300

    structuring data frontend

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -8,5 +8,23 @@ module.exports = {
     const products = results.rows
 
     if(!products) return res.send("Products not found!")
+
+    async function getImage(productId) {
+      let results = await Product.files(product.id)
+      const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public","")}`)
+
+      return files[0]
+    }
+
+    const productsPromise = products.map(product => {
+      product.img = await getImage(product)
+      product.oldPrice = formatPrice(product.old_price)
+      product.price = formatPrice(product.price)
+      return product
+    })
+
+    const lastAdded = await Promise.all(productsPromise)
+
+    return res.render("home/index", { products: lastAdded })
   }
 }
