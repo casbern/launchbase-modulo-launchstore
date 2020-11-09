6ec903f74553c5fa39f1d71199f84b97efc7df45
Cássia <cascrisbern@gmail.com>
2020-11-09 16:47:36 -0300

    structuring search page

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,15 @@ const routes = express.Router()
 const multer = require('./app/middlewares/multer')
 const ProductController = require('./app/controllers/ProductController')
 const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
+// HOME
 routes.get('/', HomeController.index)
 
+// SEARCH
+routes.get('/products/search', SearchController.index)
+
+// PRODUCTS
 routes.get('/products/create', ProductController.create)
 routes.post('/products', multer.array('photos', 6), ProductController.post)
 
@@ -16,11 +22,10 @@ routes.put('/products', multer.array('photos', 6), ProductController.put)
 
 routes.delete('/products', ProductController.delete)
 
-//* Alias
+// ALIAS
 routes.get('/ads/create', function(req, res) {
   return res.redirect('/products/create')
 })
 
- 
 
 module.exports = routes
