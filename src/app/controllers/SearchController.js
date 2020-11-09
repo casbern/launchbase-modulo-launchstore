1bd9cff3ca112db3626c72032881152783ef63b7
Cássia <cascrisbern@gmail.com>
2020-11-09 17:35:33 -0300

    sql search

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -3,28 +3,49 @@ const Product = require('../models/Product')
 const File = require('../models/File')
 
 module.exports = { 
-  async index(req, res) {
-    let results = await Product.all()
-    const products = results.rows
+  async index(req,res) {
+    try {
+      let results, 
+          params = {}
+          
+      const {filter, category} = req.query
 
-    if(!products) return res.send("Products not found!")
+      if(!filter) return res.redirect("/")
 
-    async function getImage(productId) {
-      let results = await Product.files(productId)
-      const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public","")}`)
+      params.filter = filter
 
-      return files[0]
+      if(category) {
+        params.category = category
+      }
+
+      results = await Product.search(params)
+
+      async function getImage(productId) {
+        let results = await Product.files(productId)
+        const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public","")}`)
+  
+        return files[0]
+      }
+
+      const productsPromise = results.rows.map(async product => {
+        product.img = await getImage(product.id)
+        product.oldPrice = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+        return product
+      })
+
+      const products = await Promise.all(productsPromise)
+
+      const search = {
+        term: req.query.filter,
+        total: products.length
+      }
+  
+      return res.render("search/index", { products })
+  
+    } catch(err) {
+      console.log(err)
     }
-
-    const productsPromise = products.map(async product => {
-      product.img = await getImage(product.id)
-      product.oldPrice = formatPrice(product.old_price)
-      product.price = formatPrice(product.price)
-      return product
-    }).filter( (product, index) => index > 2 ? false : true)
-
-    const lastAdded = await Promise.all(productsPromise)
-
-    return res.render("search/index", { products: lastAdded })
   }
 }
+  
