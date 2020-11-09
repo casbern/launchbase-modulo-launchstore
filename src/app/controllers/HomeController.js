d053b4653af3a66adc79905fa78cc7b8f45f81e8
Cássia <cascrisbern@gmail.com>
2020-11-09 16:29:13 -0300

    positioning images, changing home styles

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -10,14 +10,14 @@ module.exports = {
     if(!products) return res.send("Products not found!")
 
     async function getImage(productId) {
-      let results = await Product.files(product.id)
+      let results = await Product.files(productId)
       const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public","")}`)
 
       return files[0]
     }
 
-    const productsPromise = products.map(product => {
-      product.img = await getImage(product)
+    const productsPromise = products.map(async product => {
+      product.img = await getImage(product.id)
       product.oldPrice = formatPrice(product.old_price)
       product.price = formatPrice(product.price)
       return product
